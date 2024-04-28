82672b395cbf6cbf0a31251d0497aaaa445e067a
Betfsh <bettymegd@gmail.com>
2024-04-28 23:30:26 +0300

    notebook added

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,3 @@
+# Telecom_user_data_analysis
+
+The project involves analyzing data and user engagement in the telecommunications industry. It includes tasks such as reviewing source code, analyzing user behavior, conducting exploratory data analysis, tracking user engagement, assessing customer satisfaction, and deploying and monitoring a machine learning model. The goal is to gain insights from the data to make informed marketing decisions and improve the user experience. Python programming language and various data analysis techniques will be used to accomplish these tasks.
--- data/data.py
+++ data/data.py
@@ -1,17 +1,26 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-database_name = 'Telecom'
-table_name = 'xdr_data'
-
-connection_params = {
-    "host": "localhost",
-    "user": "postgres",
-    "password": "pogres",
-    "port": "5432",
-    "database": database_name}
-
-engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
-
-sql_query = 'SELECT * FROM xdr_data LIMIT 5'
-df = pd.read_sql(sql_query, con=engine)
+def df_from_database(chunksize=None):
+    database_name = 'Telecom'
+    table_name = 'xdr_data'
+    
+    connection_params = {
+        "host": "localhost",
+        "user": "postgres",
+        "password": "pogres",
+        "port": "5432",
+        "database": database_name
+    }
+    
+    engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
+    
+    sql_query = f"SELECT * FROM {table_name}"
+    
+    if chunksize:
+        df_chunks = pd.read_sql(sql_query, con=engine, chunksize=chunksize)
+        df = pd.concat(df_chunks)
+    else:
+        df = pd.read_sql(sql_query, con=engine)
+    
+    return df
